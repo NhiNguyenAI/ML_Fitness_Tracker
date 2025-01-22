404c78ee6f2f4a3af61172b7325fc6b81083907d
NhiNguyenAI <nhintnguyenAI@gmail.com>
2025-01-22 21:30:44 +0100

    created the function to read the files

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -108,10 +108,62 @@ del gyroscopes_df["elapsed (s)"]
 # Turn into function
 # --------------------------------------------------------------
 
+files = glob("../../data/raw/MetaMotion/*.csv")
+def read_data_files (files):
+
+    # Extract features from filename  
+    data_path = "../../data/raw/MetaMotion/"
+
+    # Read all files
+    accelerometer_df = pd.DataFrame()
+    gyroscopes_df = pd.DataFrame()
+
+    accelerometer_set = 1
+    gyroscopes_set = 1
+
+    for f in files:
+        participant = f.split("-")[0].replace(data_path,"") 
+        label = f.split("-")[1]
+        category = f.split("-")[2].rstrip("123").rstrip("_MetaWear_2019")
+        if "Accelerometer" in f:
+            sensor = "Accelerometer"
+        if "Gyroscope" in f:
+            sensor = "Gyroscope"
+
+        df = pd.read_csv(f)
+        df["participant"]= participant
+        df["label"]= label
+        df["category"]= category
+        df["sensor"]= sensor
+
+        if "Accelerometer" in f:
+            df["set"] = accelerometer_set
+            accelerometer_set += 1
+            accelerometer_df = pd.concat([accelerometer_df, df])
+        if "Gyroscope" in f:
+            df["set"] = gyroscopes_set
+            gyroscopes_set += 1
+            gyroscopes_df = pd.concat([gyroscopes_df, df])
+
+    # Working with datetimes for this data
+    accelerometer_df.index = pd.to_datetime(accelerometer_df["epoch (ms)"], unit= "ms")
+    gyroscopes_df.index = pd.to_datetime(gyroscopes_df["epoch (ms)"], unit= "ms")
+
+    # delete column epoch, time (01:00) and elapsed, because we dont use the column more
+    del accelerometer_df["epoch (ms)"]
+    del accelerometer_df["time (01:00)"]
+    del accelerometer_df["elapsed (s)"]
+
+    del gyroscopes_df["epoch (ms)"]
+    del gyroscopes_df["time (01:00)"]
+    del gyroscopes_df["elapsed (s)"]
+
+    return(accelerometer_df, gyroscopes_df)
 
 # --------------------------------------------------------------
 # Merging datasets
 # --------------------------------------------------------------
+
 
 
 # --------------------------------------------------------------
