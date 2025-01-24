48ddb4746ec8a854f843775e775b266086966227
NhiNguyenAI <nhintnguyenAI@gmail.com>
2025-01-24 22:24:24 +0100

    created the remove outliers method

--- document/doc/Process_Develop_ML_Project.md
+++ document/doc/Process_Develop_ML_Project.md
@@ -30,11 +30,18 @@ In this step: Focus in the remaining steps
 **Step4**: Adjust plot settings with mpl
 
 **Step5**: Plot multiple axis: Display all variables (e.g.: heavy and medium) of a parameter (e.g.: category) in a diagram.
-**Note1**: If the diagram get messy, then use sort_values -> participant_df = df.query("label =='bench'").**sort_values**("participant").reset_index()
-**Note2**: for the multiple axis: all_axis_df **[** "acc_x","acc_y","acc_x","acc_z","gyr_x","gyr_y","gyr_z" **]** is Series, can't plot -> all_axis_df **[[** "acc_x","acc_y","acc_x","acc_z","gyr_x","gyr_y","gyr_z" **]]**
+    **- Note1**: If the diagram get messy, then use sort_values -> participant_df = df.query("label =='bench'").**sort_values**("participant").reset_index()
+    **- Note2**: for the multiple axis: all_axis_df **[** "acc_x","acc_y","acc_x","acc_z","gyr_x","gyr_y","gyr_z" **]** is Series, can't plot -> all_axis_df **[[** "acc_x","acc_y","acc_x","acc_z","gyr_x","gyr_y","gyr_z" **]]**
 
 **Step6**: Export the data for each label following each participant in PNG file
 
 ## Step 4: Detecting outliers in sensor data
+
+**Step1** Plotting outliers
+**Step2** Interquartile range (IQR)
+    **- Note1**:  take careful, working now on dataframe -> plot with boxplot: **type(df[["acc_x"]]) # dataframe** and **type(df["acc_x"])   # Series** 
+
+
+
 
 
--- src/features/remove_outliers.py
+++ src/features/remove_outliers.py
@@ -3,16 +3,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import scipy
-from sklearn.neighbors import LocalOutlierFactor  # pip install scikit-learn
+from sklearn.neighbors import LocalOutlierFactor
 
 # --------------------------------------------------------------
-# Load data
+# 1. Load data
+# --------------------------------------------------------------
+df = pd.read_pickle("../../data/interim/data_process.pkl")
+
+# --------------------------------------------------------------
+# 2. Plotting outliers
 # --------------------------------------------------------------
 
+# --------------------------------------------------------------
+# 2.1 Adjust plot settings
+# --------------------------------------------------------------
+plt.style.use("seaborn-v0_8-deep")
+plt.rcParams['figure.figsize'] = (20,5)
+plt.rcParams['figure.dpi'] = 100
 
-# --------------------------------------------------------------
-# Plotting outliers
-# --------------------------------------------------------------
+# take careful, working now on dataframe -> plot with boxplot
+type(df[["acc_x"]]) # dataframe
+type(df["acc_x"])   # Series
+
+df[["acc_x"]].boxplot()
+
+# boxplot of acc_x following the label
+
+df[["acc_x", "label"]].boxplot(by = "label", figsize = (20,10))
+
+# looking for multi axis at the same time, and plot boxplot
+columns_outliers = df.columns[:6] 
+# columns_outliers is now included with index -> list()
+columns_outliers = list(df.columns[:6])
+fig, axes = plt.subplots(1, 3, figsize=(20, 10))
+# for acc
+df[columns_outliers[:3] + ["label"]].boxplot(by= "label", ax=axes, layout=(1, 3))
+
+# for gyr
+fig, axes = plt.subplots(1, 3, figsize=(20, 10))
+df[columns_outliers[3:6] + ["label"]].boxplot(by= "label", ax=axes, layout=(1, 3))
 
 
 # --------------------------------------------------------------
