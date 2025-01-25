fe29f41255f5708f67bc999fe0cf4ca51d189d94
NhiNguyenAI <nhintnguyenAI@gmail.com>
2025-01-25 21:42:09 +0100

    created new file for the develop process of detecting outliers of data sensors

--- src/features/detect_outliers/remove_outliers_dev_process_test.py
+++ src/features/detect_outliers/remove_outliers_dev_process_test.py
@@ -4,8 +4,8 @@ import matplotlib.pyplot as plt
 import math
 import scipy
 from sklearn.neighbors import LocalOutlierFactor
-import Plotting_otliers_in_time
-import mark_outliers_schauvenet
+import function.outliers.Plotting_outliers_in_time as Plotting_outliers_in_time
+import function.outliers.mark_outliers_iqr as mark_outliers_iqr
 
 # --------------------------------------------------------------
 # 1. Load data
@@ -37,11 +37,13 @@ df[["acc_x", "label"]].boxplot(by = "label", figsize = (20,10))
 columns_outliers = df.columns[:6] 
 # columns_outliers is now included with index -> list()
 columns_outliers = list(df.columns[:6])
+
 fig, axes = plt.subplots(1, 3, figsize=(20, 10))
 # for acc
 df[columns_outliers[:3] + ["label"]].boxplot(by= "label", ax=axes, layout=(1, 3))
 save_path = "../../reports/figures/interquartile_range/acc.png"
 plt.savefig(save_path)
+
 # for gyr
 fig, axes = plt.subplots(1, 3, figsize=(20, 10))
 df[columns_outliers[3:6] + ["label"]].boxplot(by= "label", ax=axes, layout=(1, 3))
@@ -50,18 +52,18 @@ plt.savefig(save_path)
 
 # above this diagramm have alot of outliers data can't viasualze#
 # Solusion: Use customer function to visualize the outliers in time
-Plotting_otliers_in_time.plot_binary_outliers()
+Plotting_outliers_in_time.plot_binary_outliers()
 
 
 # --------------------------------------------------------------
 # Interquartile range (distribution based)
 # --------------------------------------------------------------
 # Insert IQR function
-mark_outliers_schauvenet.mark_outliers_chauvenet()
+mark_outliers_iqr.mark_outliers_iqr()
 
 # Plot a single column
 col = "acc_x"
-mark_outliers_schauvenet(df,col)
+mark_outliers_iqr_df= mark_outliers_iqr.mark_outliers_iqr(df,col)
 
 # Loop over all columns
 
