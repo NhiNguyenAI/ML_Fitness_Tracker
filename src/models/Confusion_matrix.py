cc6810051c596c25f40e58bf56877f19d93610bc
NhiNguyenAI <nhintnguyenAI@gmail.com>
2025-02-11 00:13:30 +0100

    choose the best model after plot the best features of the model

---
+++ src/models/Confusion_matrix.py
@@ -0,0 +1,22 @@
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
--- src/models/train_model.py
+++ src/models/train_model.py
@@ -88,7 +88,8 @@ selected_features, ordered_features, ordered_scores = learner.forward_selection(
 
 # After the Selection of the best features, we can plot the best features of the model
 # Selected _features = ordered_scores
-selected_features = [   'acc_z_freq_0.0_Hz_ws_14',
+selected_features = [   
+                        'acc_z_freq_0.0_Hz_ws_14',
                         'acc_x_freq_0.0_Hz_ws_14',
                         'duration',
                         'acc_y_freq_0.0_Hz_ws_14',
@@ -119,11 +120,13 @@ possible_feature_sets = [
                             selected_features
                         ]
 
-feature_names = ["set_festure_1",
+feature_names = [
+                  "set_festure_1",
                   "set_festure_2",
                   "set_festure_3",
                   "set_festure_4",
-                  "selected_features"]
+                  "selected_features"
+                ]
 
 # Number of iterations for training
 iterations = 1
@@ -231,16 +234,17 @@ plt.ylim(0.7, 1)
 plt.legend(loc="lower right")
 plt.show()
 
-
+# choose the best model after plot the best features of the model
 (
     class_train_y,
     class_test_y,
     class_train_prob_y,
     class_test_prob_y,
 ) = learner.random_forest(
-            selected_train_X, y_train, selected_test_X, gridsearch=True
+            X_train[selected_features], y_train, X_test[selected_features], gridsearch=True
 )
 
+accuracy= accuracy_score(y_test, class_test_y)
 
 # --------------------------------------------------------------
 # Select best model and evaluate results
