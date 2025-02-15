9430f026536053931ac7b1c6643c67d160bc6cb4
NhiNguyenAI <nhintnguyenAI@gmail.com>
2025-02-15 22:23:22 +0100

    Create confusion matrix

--- src/models/train_model.py
+++ src/models/train_model.py
@@ -246,6 +246,33 @@ plt.show()
 
 accuracy= accuracy_score(y_test, class_test_y)
 
+# Create confusion matrix
+classes  = class_test_prob_y.columns
+cm = confusion_matrix(y_test, class_test_y, labels = classes)
+
+# create confusion matrix for cm
+plt.figure(figsize=(10, 10))
+plt.imshow(cm, interpolation="nearest", cmap=plt.cm.Blues)
+plt.title("Confusion matrix")
+plt.colorbar()
+tick_marks = np.arange(len(classes))
+plt.xticks(tick_marks, classes, rotation=45)
+plt.yticks(tick_marks, classes)
+
+thresh = cm.max() / 2.0
+for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+    plt.text(
+        j,
+        i,
+        format(cm[i, j]),
+        horizontalalignment="center",
+        color="white" if cm[i, j] > thresh else "black",
+    )
+plt.ylabel("True label")
+plt.xlabel("Predicted label")
+plt.grid(False)
+plt.show()
+
 # --------------------------------------------------------------
 # Select best model and evaluate results
 # --------------------------------------------------------------
